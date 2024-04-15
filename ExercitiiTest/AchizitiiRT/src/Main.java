451fc861f2d241b869f675c8fc73bdff6972335b
Tudose Razvan <146849110+TudoseRazvan@users.noreply.github.com>
2024-04-15 23:11:51 +0300

    Update Main.java

--- ExercitiiTest/AchizitiiRT/src/Main.java
+++ ExercitiiTest/AchizitiiRT/src/Main.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //Initializare pentru clasa Data
